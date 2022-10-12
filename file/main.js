e8de233efa1cf85aa99ac80cb4eaf6160abfad74
Dmutre <drago.modern@gmail.com>
2022-10-12 22:21:10 +0300

    0.3

--- file/main.js
+++ file/main.js
@@ -1,3 +1,20 @@
-let str = prompt("Pussy: ");
-console.log("Pussy", str);
-document.write("Hello Visual Studio Code");
+"use strict";
+
+const MAX_VALUE = 5;
+let counter = 0;
+const interval = 3000;
+let timer;
+
+
+
+function event(){
+    console.log(new Date());
+    counter++;
+    if(counter == MAX_VALUE){
+        clearInterval(timer);
+        alert("The end");
+    }
+}
+
+alert("Begin");
+timer = setInterval(event, interval);
