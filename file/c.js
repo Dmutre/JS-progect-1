cd8c932f9b7c1949b34b4e761d961a112fdf0f38
Dmutre <dmytro.lesko@gmail.com>
2022-11-24 12:07:54 +0300

    test

--- file/c.js
+++ file/c.js
@@ -2,3 +2,5 @@
 
 let a = "00000000000000000000000000000000000000000000000000000000000000000000000000000000";
 console.log(a.length);
+console.log(a.length);
+console.log("Never gonna give you up,...");
